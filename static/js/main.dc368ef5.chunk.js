9fd0d3c0bc9a8f48c8661548ce81af3233f5b111
ciamac-da <ciamac.davoudi@gmail.com>
2021-08-12 15:02:05 +0200

    Updates

--- static/js/main.dc368ef5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-todo-app-47"]=this["webpackJsonpreact-todo-app-47"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var c=n(0),r=n.n(c),o=n(3),a=n.n(o),s=(n(8),n(1));var p=function(){return Object(s.jsx)(c.Fragment,{})};a.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(p,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.dc368ef5.chunk.js.map
